3f793748cbc87f0f0b478e252b4e9f7b42a7d4e0
sophiedabi <95542937+sophiedabi@users.noreply.github.com>
2022-04-22 20:04:03 +0200

    changed structure

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
+        Listen on click
       </a>
       <span className="text">{props.phonetic.text}</span>
     </div>
